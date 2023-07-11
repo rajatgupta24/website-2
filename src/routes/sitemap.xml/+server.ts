b186f4b7b25748449779beca3c538bf643e76c74
Siddhant Khare <siddhant@gitpod.io>
2023-07-11 10:34:18 +0530

    🛠️ Rewrite sitemap for better URLs (#3836)

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,5 +1,11 @@
 import { removeTrailingSlash } from '$lib/utils/helpers';
 import type { RequestHandler } from './$types';
+import { listBlogPosts } from '$content/blog';
+import { listGuides } from '$content/guides';
+import { listEducationCustomers } from '$content/education';
+import { listDocsSlugs } from '$content/docs';
+import { listChangelogs, getChangelogSlug } from '$content/changelog';
+import { listCustomers } from '$content/customers';
 
 // prettier-ignore
 const sitemap = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
@@ -10,15 +16,7 @@ const sitemap = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
 		xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
 		xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
 		xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-	>
-		<url>
-			<loc>https://www.gitpod.io</loc>
-		</url>
-		
-		<url>
-			<loc>${removeTrailingSlash('https://www.gitpod.io/docs/introduction/getting-started')}</loc>
-		</url>
-
+	>		
 		${pages.map((page) => `<url><loc>${removeTrailingSlash(page)}</loc></url>`).join('')}
 	</urlset>
 `;
@@ -56,27 +54,37 @@ export const GET: RequestHandler = async () => {
 				.replace('/+page', '');
 		});
 
-	const contentPages = Object.keys(
-		// For Changelogs, Blogs, and Guides
-		import.meta.glob('/src/content/**/!(_)*.{svelte,md}', {
-			eager: true,
-		}),
-	)
-		.filter((page) => {
-			const filters = ['_', 'slug]', 'title]', '+error', '+layout'];
-			return !filters.find((filter) => page.includes(filter));
-		})
-		.map((page) => {
-			return page
-				.replace('/src/content', 'https://www.gitpod.io')
-				.replace('/index.md', '/')
-				.replace('.md', '/')
-				.replace('/index.svelte', '/')
-				.replace('.svelte', '/')
-				.replace('/+page', '');
-		});
+	const blogPosts = listBlogPosts().map(
+		(post) => `https://www.gitpod.io/blog/${post.slug}`,
+	);
+	const guidePosts = listGuides().map(
+		(post) => `https://www.gitpod.io/guides/${post.slug}`,
+	);
+	const educationPosts = listEducationCustomers().map(
+		(post) => `https://www.gitpod.io/discover/education/${post.slug}`,
+	);
+	const docsPosts = listDocsSlugs().map(
+		(slug) => `https://www.gitpod.io/docs/${slug}`,
+	);
+	const changeLogs = listChangelogs().map(
+		(post) =>
+			`https://www.gitpod.io/changelog/${getChangelogSlug(
+				post.metadata,
+			)}`,
+	);
+	const customersPages = listCustomers().map(
+		(post) => `https://www.gitpod.io/customers/${post.slug}`,
+	);
 
-	const renderedSitemap = sitemap([...staticPages, ...contentPages]);
+	const renderedSitemap = sitemap([
+		...staticPages,
+		...blogPosts,
+		...guidePosts,
+		...educationPosts,
+		...docsPosts,
+		...changeLogs,
+		...customersPages,
+	]);
 
 	return new Response(renderedSitemap, {
 		headers: {
